f89c3d8a782ed0a3341dfd1e3a99a39096a05f03
Sylvain Leroux <sylvain@chicoree.fr>
2015-12-15 15:51:53 +0100

    Converted controller to $scope

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict';
 angular.module('confusionApp', [])
 
-        .controller('MenuController', function() {
-            var dishes=[
+        .controller('MenuController', ['$scope', function($scope) {
+            $scope.dishes = [
                          {
                            name:'Uthapizza',
                            image: 'images/uthapizza.png',
@@ -41,31 +41,29 @@ angular.module('confusionApp', [])
                         }
                         ];
 
-            this.dishes = dishes;
+            $scope.tab = 1;
+            $scope.filtText = '';
 
-            this.tab = 1;
-            this.filtText = '';
-
-            this.select = function(setTab) {
-                this.tab = setTab;
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
 
                 if (setTab === 2) {
-                    this.filtText = "appetizer";
+                    $scope.filtText = "appetizer";
                 }
                 else if (setTab === 3) {
-                    this.filtText = "mains";
+                    $scope.filtText = "mains";
                 }
                 else if (setTab === 4) {
-                    this.filtText = "dessert";
+                    $scope.filtText = "dessert";
                 }
                 else {
-                    this.filtText = "";   
+                    $scope.filtText = "";   
                 }
             };
 
-            this.isSelected = function (checkTab) {
-                return (this.tab === checkTab);
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
             };
 
 
-        });
+        }]);
