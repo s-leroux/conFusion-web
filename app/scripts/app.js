f3b5700133e03f910ad15e37dc86ab82db14da6d
Sylvain Leroux <sylvain@chicoree.fr>
2015-12-16 19:46:36 +0100

    Assignment 3 - Factored out controller in app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -97,4 +97,73 @@ angular.module('confusionApp', [])
                 }
             };
         }])
+
+        .controller('DishDetailController', function() {
+            var dish={
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains', 
+                          label:'Hot',
+                          price:'4.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                           comments: [
+                               {
+                                   rating:5,
+                                   comment:"Imagine all the eatables, living in conFusion!",
+                                   author:"John Lemon",
+                                   date:"2012-10-16T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                                   author:"Paul McVites",
+                                   date:"2014-09-05T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:3,
+                                   comment:"Eat it, just eat it!",
+                                   author:"Michael Jaikishan",
+                                   date:"2015-02-13T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Ultimate, Reaching for the stars!",
+                                   author:"Ringo Starry",
+                                   date:"2013-12-02T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:2,
+                                   comment:"It's your birthday, we're gonna party!",
+                                   author:"25 Cent",
+                                   date:"2011-12-02T17:57:28.556094Z"
+                               }
+                               
+                           ]
+                    };
+            
+            this.dish = dish;
+            
+        })
+        
+        // ASSIGNMENT 3
+        .controller('CommentFormController', function() {
+            /*
+              I didn't use $scope to solve this problem as they will not be supported
+              in Angular 2 according to this discution on the forum:
+
+              https://www.coursera.org/learn/angular-js/module/J5XIt/discussions/YSZJ6p6LEeWGxBJdkUHhbw
+            */
+            this.comment = { comment: "", rating: 5, author: "", date: null };
+
+            this.sendComment = function(dish) {
+              this.comment.date = new Date();
+              dish.comments.push(this.comment);
+              this.comment = { comment: "", rating: 5, author: "", date: null };
+            };
+
+            this.reset = function(form) {
+              form.$setPristine();
+            };
+        })
+
         ;
